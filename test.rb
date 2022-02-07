f7f2b57cdd172f8ee1052d67a868ad235a341868
EhabFayed <ehabahmed2233@gmail.com>
2022-02-07 15:43:57 +0200

    first commit

---
+++ test.rb
@@ -0,0 +1,13 @@
+!/usr/bin/env ruby
+print "hello world\n"
+puts "Hello world!"
+p "hello world!"
+greeting = "Hello world!"
+puts greeting
+puts "inter ur name "
+input = gets.chomp
+puts input.length
+puts "thenaks for inter your {input}"
+puts "please inter number to multiply by 2 "
+nimmber = gets.chomp
+puts nimmber.to_i * 2
